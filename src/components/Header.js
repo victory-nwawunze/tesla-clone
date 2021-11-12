0194b26f92d478081ffc3bdc9473e66c67c89582
unknown <victorynwawunze@gmail.com>
2021-11-12 07:08:14 -0800

    adding tesla icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,26 @@
 /** @format */
 
 import React from "react";
-
+import styled from "styled-components";
 function Header() {
-  return <div>header</div>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu></Menu>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  padding: 0 20px;
+  align-item: center;
+`;
+
+const Menu = styled.div``;

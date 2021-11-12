1d24146f6765835884d0e4c2164d5f05cf6c4e82
unknown <victorynwawunze@gmail.com>
2021-11-12 08:42:14 -0800

    created the header section

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,29 @@
 
 import React from "react";
 import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+// import AiOutlineMenu from "react-icons";
 function Header() {
   return (
     <Container>
       <a>
         <img src="/images/logo.svg" alt="" />
       </a>
-      <Menu></Menu>
+      <Menu>
+        <a href="#">Model S</a>
+
+        <a href="#">Model 3</a>
+
+        <a href="#">Model X</a>
+
+        <a href="#">Model Y</a>
+      </Menu>
+
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+        <CustomMenu />
+      </RightMenu>
     </Container>
   );
 }
@@ -21,7 +37,38 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   padding: 0 20px;
-  align-item: center;
+  align-items: center;
+  top: 0;
+  left: 0;
+  right: 0;
+  justify-content: space-between;
 `;
 
-const Menu = styled.div``;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    margin-right: 10px;
+  }
+`;
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;

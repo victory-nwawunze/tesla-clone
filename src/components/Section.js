5737b3ec8dcfc6a26e392caaecec2739e07b5b9a
unknown <victorynwawunze@gmail.com>
2021-11-12 05:01:57 -0800

    Just created the Down animation button effect

--- src/components/Section.js
+++ src/components/Section.js
@@ -58,6 +58,7 @@ const LeftButton = styled.div`
   text-transform: uppercase;
   font-size: 12px;
   cursor: pointer;
+  margin: 8px;
 `;
 
 const RightButton = styled(LeftButton)``;
@@ -65,6 +66,7 @@ const RightButton = styled(LeftButton)``;
 const DownArrow = styled.img`
   margin-top: 20px;
   height: 40px;
+  animation: animateDown infinite 1.5s;
 `;
 
 const Buttons = styled.div``;

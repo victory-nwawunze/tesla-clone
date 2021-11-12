ead12a447113767e5b19cc546d3d379ae3134456
unknown <victorynwawunze@gmail.com>
2021-11-12 04:34:24 -0800

    Just added to LeftButton

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,6 +9,10 @@ function Section() {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
+      <ButtonGroup>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Existing Invertory</RightButton>
+      </ButtonGroup>
     </Wrap>
   );
 }
@@ -22,9 +26,33 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //horizonal
 `;
 
 const ItemText = styled.div`
   padding-top: 15vh;
-  text-align: center;
+  text-aligns: center;
 `;
+
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+`;
+
+const RightButton = styled(LeftButton)``;

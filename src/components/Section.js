53ff7f6c010583a92b054da1c1a5c6b138d13776
unknown <victorynwawunze@gmail.com>
2021-11-12 09:03:35 -0800

    added react reveal animation

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 function Section({
   title,
   description,
@@ -11,15 +12,19 @@ function Section({
 }) {
   return (
     <Wrap bgImage={backgroudImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
       <Buttons>
-        <ButtonGroup>
-          <LeftButton>{leftBtnText}</LeftButton>
-          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
-        </ButtonGroup>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftButton>{leftBtnText}</LeftButton>
+            {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+          </ButtonGroup>
+        </Fade>
         <DownArrow src="/images/down-arrow.svg" />
       </Buttons>
     </Wrap>
